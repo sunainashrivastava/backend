e40de2a0588c393299179a4cda18f7483e6a417f
sunainashrivastava <shrivastavasunaina1@gmail.com>
2022-03-27 16:58:50 +0530

    Added sorting and parsing functionality

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
+require './app/filter'
+require 'byebug'
 class PeopleController
   def initialize(params)
     @params = params
   end
 
   def normalize
+    Filter.new(params).result if params.any?
   end
 
   private
